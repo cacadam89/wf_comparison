53ae1bb10ccf699f4d270d982644e19edd4f720b
Adam Caccavale <adam.w.caccavale@gmail.com>
2019-12-30 23:37:00 -0500

    Initial commit

---
+++ docker/x86/wf_compare_entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+# setup ros environment
+source "/opt/ros/$ROS_DISTRO/setup.bash"
+
+# start avahi daemon
+/etc/init.d/dbus start &>/dev/null
+service avahi-daemon start &>/dev/null
+
+# source the cartographer-specific env
+source ~/wf_compare_ws/install_isolated/setup.bash
+
+exec "$@"
