20c0017ae37b48f0fef566459de7766a2b251889
Adam Caccavale <adam.w.caccavale@gmail.com>
2020-01-03 14:40:39 -0800

    Added CMakeLists etc

--- docker/x86/wf_comparison_entrypoint.sh
+++ docker/x86/wf_comparison_entrypoint.sh
@@ -9,6 +9,6 @@ source "/opt/ros/$ROS_DISTRO/setup.bash"
 service avahi-daemon start &>/dev/null
 
 # source the cartographer-specific env
-source ~/wf_compare_ws/install_isolated/setup.bash
+source ~/wf_comparison_ws/install_isolated/setup.bash
 
 exec "$@"
